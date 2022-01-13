af70c974268341d645f5ddc24f30fbd8fec66eda
Fed333 <kovalchuk.roman03@gmail.com>
2022-01-13 21:40:31 +0200

    added current session Locale

--- src/main/java/com/epam/cargo/controller/InformationController.java
+++ src/main/java/com/epam/cargo/controller/InformationController.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Locale;
+
 @Controller
 public class InformationController {
 
@@ -20,11 +22,12 @@ public class InformationController {
     @GetMapping("/")
     public String information(
             Model model,
-            @PageableDefault(sort = {"senderCity.name"}, direction = Sort.Direction.ASC) Pageable pageable
+            @PageableDefault(sort = {"senderCity.name"}, direction = Sort.Direction.ASC) Pageable pageable,
+            Locale locale
     ){
         Page<DirectionDelivery> directionsPage;
 
-        directionsPage = directionDeliveryService.findAll(pageable);
+        directionsPage = directionDeliveryService.findAll(pageable, locale);
 
         model.addAttribute("directionsPage", directionsPage);
         model.addAttribute("url", "/");
