7a8a97d7aff0eeaa393ea10ec423c7af31d3e761
Fed333 <kovalchuk.roman03@gmail.com>
2022-01-14 17:58:11 +0200

    added formSubmit.js for providing useful js functions

--- src/main/resources/static/js/formSubmit.js
+++ src/main/resources/static/js/formSubmit.js
@@ -1,15 +1,42 @@
 
 function formSubmit(formId){
-    console.log("formId: " + formId)
+//    console.log("formId: " + formId)
     document.getElementById(formId).submit()
 }
 
 function setSortOptionSelected(optionId, expectedValue){
+//    console.log('setSortOptionSelected has been called')
     option = document.getElementById(optionId)
-    console.log("option.value: " + option.value + ", expectedValue: " + expectedValue)
+//    console.log("option.value: " + option.value + ", expectedValue: " + expectedValue)
 
-    if (option.value === expectedValue){
+    if (option.value.toLowerCase() === expectedValue.toLowerCase()){
         option.setAttribute('selected', 'selected')
-        console.log("added selected attribute to " + optionId)
+//        console.log("added selected attribute to " + optionId)
     }
+}
+
+function setSortOptionOrder(sortSelectOrderId, sortSelectCriterionId){
+//    console.log('setSortOptionOrder has been called')
+
+    selectCriterion = document.getElementById(sortSelectCriterionId)
+    optionCriterion = selectCriterion.options[selectCriterion.selectedIndex]
+
+    selectOrder = document.getElementById(sortSelectOrderId)
+
+    optionOrder = selectOrder.options[selectOrder.selectedIndex]
+    value = optionCriterion.getAttribute('value')
+    value = value.replace(/(,[a-zA-Z]*)/, '')
+    value = value + "," + optionOrder.getAttribute('value')
+
+//    console.log("value: " + value)
+
+    optionCriterion.setAttribute('value', value)
+}
+
+function setPageNumber(pageNumber){
+    document.getElementById('pageNumber').setAttribute('value', pageNumber)
+}
+
+function setPageSize(pageSize){
+    document.getElementById('pageSize').setAttribute('value', pageSize)
 }
