1029fc716f3e464cb6b5d9f7847d0af85db0aa93
Fed333 <kovalchuk.roman03@gmail.com>
2022-01-21 01:44:33 +0200

    added registration business logic to UserService

--- src/main/java/com/epam/cargo/service/UserService.java
+++ src/main/java/com/epam/cargo/service/UserService.java
@@ -1,19 +1,30 @@
 package com.epam.cargo.service;
 
+import com.epam.cargo.dto.AddressRequest;
+import com.epam.cargo.dto.UserRequest;
+import com.epam.cargo.entity.Address;
+import com.epam.cargo.entity.City;
 import com.epam.cargo.entity.Role;
 import com.epam.cargo.entity.User;
+import com.epam.cargo.exception.*;
 import com.epam.cargo.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UserService implements UserDetailsService {
+
+    @Value("${validation.password.regexp}")
+    private String passwordValidRegex;
+
+    @Value("${spring.messages.basename}")
+    private String messages;
 
     @Autowired
     private UserRepo userRepo;
@@ -21,21 +32,105 @@ public class UserService implements UserDetailsService {
     @Autowired
     private AddressService addressService;
 
-    public boolean addUser(User user){
+    @Autowired
+    private CityService cityService;
+
+
+    public User registerUser(UserRequest userRequest, Locale locale) throws WrongDataException {
+        ResourceBundle bundle = ResourceBundle.getBundle(messages, locale);
+        User user = new User();
+        initializePersonalData(userRequest, user);
+        initializeCredentials(userRequest, user, bundle);
+
+        this.addUser(user);
+        return user;
+    }
+
+    private void initializeCredentials(UserRequest userRequest, User user, ResourceBundle bundle) throws WrongDataException {
+        String login = userRequest.getLogin();
+        requireUniqueLogin(login, bundle);
+        user.setLogin(login);
+
+        String password = userRequest.getPassword();
+        requireValidPassword(password, bundle);
+        requirePasswordDuplicationMatch(userRequest, password, bundle);
+        user.setPassword(password);
+    }
+
+    private void requirePasswordDuplicationMatch(UserRequest userRequest, String password, ResourceBundle bundle) throws DuplicatePasswordValidationException {
+        if (!password.equals(userRequest.getDuplicatePassword())){
+            throw new DuplicatePasswordValidationException(bundle);
+        }
+    }
+
+    private void requireValidPassword(String password, ResourceBundle bundle) throws NoValidPasswordException {
+        Objects.requireNonNull(password);
+        if (!isValidPassword(password)){
+            throw new NoValidPasswordException(bundle);
+        }
+    }
+
+    private boolean isValidPassword(String password) {
+        return password.matches(passwordValidRegex);
+    }
+
+    private void requireUniqueLogin(String login, ResourceBundle bundle) throws OccupiedLoginException {
+        if (!Objects.isNull(userRepo.findByLogin(login))){
+            throw new OccupiedLoginException(bundle, login);
+        }
+    }
+
+    private void initializePersonalData(UserRequest userRequest, User user) throws WrongDataException{
+        user.setName(userRequest.getName());
+        user.setSurname(userRequest.getSurname());
+        user.setEmail(userRequest.getEmail());
+        user.setPhone(userRequest.getPhone());
+
+        assignAddressToUser(userRequest, user);
+    }
+
+    private boolean assignAddressToUser(UserRequest userRequest, User user) throws NoExistingCityException {
+        AddressRequest addressRequest = userRequest.getAddress();
+        if (!Objects.isNull(addressRequest)) {
+
+            City city = cityService.findCityById(addressRequest.getCityId());
+            if (!Objects.isNull(city)){
+                Address address = new Address();
+                address.setCity(city);
+                address.setStreet(addressRequest.getStreetName());
+                address.setHouseNumber(addressRequest.getHouseNumber());
+
+                addressService.addAddress(address);
+                user.setAddress(address);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addUser(User user) throws NoExistingCityException {
+        if (Objects.isNull(user)){
+            return false;
+        }
         User foundUser;
-        if ((foundUser = userRepo.findByLogin(user.getLogin())) != null){
+        if (!Objects.isNull(foundUser = userRepo.findByLogin(user.getLogin()))){
             user.setId(foundUser.getId());
             return false;
         }
         user.setRoles(Collections.singleton(Role.USER));
-        Optional.ofNullable(user.getAddress()).ifPresent(addressService::addAddress);
+
+        Address address = user.getAddress();
+        if (!Objects.isNull(address)) {
+            addressService.addAddress(address);
+        }
+
         userRepo.save(user);
 
         return true;
     }
 
     public User findUserById(Long id){
-        return userRepo.findById(id).orElseThrow();
+        return userRepo.findById(id).orElse(null);
     }
 
     @Override
