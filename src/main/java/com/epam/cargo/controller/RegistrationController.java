dee3e057f8db260827074c85e4d365971bf571f0
Fed333 <kovalchuk.roman03@gmail.com>
2022-01-21 02:22:39 +0200

    added RegistrationController for registration

---
+++ src/main/java/com/epam/cargo/controller/RegistrationController.java
@@ -0,0 +1,44 @@
+package com.epam.cargo.controller;
+
+import com.epam.cargo.dto.UserRequest;
+import com.epam.cargo.exception.WrongDataException;
+import com.epam.cargo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Locale;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String registrationPage(
+            Model model
+    ){
+        return "registration";
+    }
+
+    @PostMapping
+    public String registration(
+            UserRequest userRequest,
+            Locale locale,
+            Model model
+    ) {
+        try {
+            userService.registerUser(userRequest, locale);
+        }
+        catch (WrongDataException e) {
+            model.addAttribute(e.getModelAttribute(), e.getMessage());
+            return "registration";
+        }
+        return "redirect:/login";
+    }
+}
