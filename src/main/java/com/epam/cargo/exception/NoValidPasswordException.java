435f0413ee78165e2d93a579f7ba367173a333d5
Fed333 <kovalchuk.roman03@gmail.com>
2022-01-23 01:32:03 +0200

    updated password validation

--- src/main/java/com/epam/cargo/exception/NoValidPasswordException.java
+++ src/main/java/com/epam/cargo/exception/NoValidPasswordException.java
@@ -1,12 +1,22 @@
 package com.epam.cargo.exception;
 
 
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class NoValidPasswordException extends WrongDataException{
-    private static final String KEY_MESSAGE = "wrong-input-data.no-valid-password";
-    public NoValidPasswordException(ResourceBundle bundle) {
-        super(bundle.getString(KEY_MESSAGE));
+    private static final String NO_FILLED_PASSWORD_KEY_ERROR_MESSAGE = "wrong-input-data.no-filled-password";
+    private static final String NO_VALID_PASSWORD_KEY_ERROR_MESSAGE = "wrong-input-data.no-valid-password";
+
+    public NoValidPasswordException(ResourceBundle bundle, String password) {
+        super(getErrorMessage(bundle, password));
+    }
+
+    private static String getErrorMessage(ResourceBundle bundle, String password) {
+        if (Objects.isNull(password) || password.isBlank()){
+            return bundle.getString(NO_FILLED_PASSWORD_KEY_ERROR_MESSAGE);
+        }
+        return bundle.getString(NO_VALID_PASSWORD_KEY_ERROR_MESSAGE);
     }
 
     @Override
