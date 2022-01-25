a3c0ba511b60269a28dc195f8ddf45ba423b60b6
Fed333 <kovalchuk.roman03@gmail.com>
2022-01-26 01:42:18 +0200

    permitted access to /fares page for unauthorized users

--- src/main/java/com/epam/cargo/config/WebSecurityConfig.java
+++ src/main/java/com/epam/cargo/config/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/login", "/registration", "/static/**").permitAll()
+                    .antMatchers("/", "/fares", "/login", "/registration", "/static/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
