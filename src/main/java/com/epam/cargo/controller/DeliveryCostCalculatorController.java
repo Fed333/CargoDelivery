7596b8575e72767da7cbc7431702fb165c238f1c
Fed333 <kovalchuk.roman03@gmail.com>
2022-01-28 22:29:56 +0200

    added validation via spring binding result

--- src/main/java/com/epam/cargo/controller/DeliveryCostCalculatorController.java
+++ src/main/java/com/epam/cargo/controller/DeliveryCostCalculatorController.java
@@ -2,19 +2,23 @@ package com.epam.cargo.controller;
 
 import com.epam.cargo.dto.DeliveryCostCalculatorRequest;
 import com.epam.cargo.dto.DeliveryCostCalculatorResponse;
-import com.epam.cargo.entity.City;
 import com.epam.cargo.exception.WrongDataException;
 import com.epam.cargo.service.CityService;
 import com.epam.cargo.service.DeliveryCostCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 @Controller
 @RequestMapping("/delivery_cost_calculator")
@@ -26,32 +30,41 @@ public class DeliveryCostCalculatorController {
     @Autowired
     private CityService cityService;
 
+    @Value("${spring.messages.basename}")
+    private String messages;
+
     @GetMapping
     public String calculatorPage(
             Model model,
-            City.Distance distance,
+            DeliveryCostCalculatorRequest calculatorRequest,
             Locale locale
     ){
-        model.addAttribute("distance", distance);
         model.addAttribute("cities", cityService.findAll(locale));
         return "deliveryCostCalculator";
     }
 
     @PostMapping
     public String calculateCost(
-            DeliveryCostCalculatorRequest calculatorRequest,
+            @Valid DeliveryCostCalculatorRequest calculatorRequest,
+            BindingResult bindingResult,
+            Model model,
             RedirectAttributes redirectAttributes,
             Locale locale
     ){
-
-        try {
-            DeliveryCostCalculatorResponse response = deliveryCostCalculatorService.calculateCost(calculatorRequest, locale);
-            redirectAttributes.addFlashAttribute("cost", response.getCost());
-            redirectAttributes.addFlashAttribute("distance", response.getDistance());
+        if (!bindingResult.hasErrors()) {
+            try {
+                DeliveryCostCalculatorResponse response = deliveryCostCalculatorService.calculateCost(calculatorRequest, locale);
+                redirectAttributes.addFlashAttribute("cost", response.getCost());
+                redirectAttributes.addFlashAttribute("distance", response.getDistance());
+            } catch (WrongDataException e) {
+                redirectAttributes.addFlashAttribute(e.getModelAttribute(), e.getMessage());
+            }
         }
-        catch (WrongDataException e){
-            redirectAttributes.addFlashAttribute(e.getModelAttribute(), e.getMessage());
+        else{
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult, ResourceBundle.getBundle(messages, locale));
+            errors.forEach(redirectAttributes::addFlashAttribute);
         }
+        redirectAttributes.addFlashAttribute("calculatorRequest", calculatorRequest);
         return "redirect:/delivery_cost_calculator";
     }
 }
