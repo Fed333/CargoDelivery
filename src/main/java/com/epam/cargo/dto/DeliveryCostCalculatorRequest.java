3014438d83fe3e6afe14ac1a275e6dec0a9bc54b
Fed333 <kovalchuk.roman03@gmail.com>
2022-01-28 22:26:02 +0200

    added data validation mechanism

--- src/main/java/com/epam/cargo/dto/DeliveryCostCalculatorRequest.java
+++ src/main/java/com/epam/cargo/dto/DeliveryCostCalculatorRequest.java
@@ -3,13 +3,28 @@ package com.epam.cargo.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import static com.epam.cargo.exception.WrongInputDataKeysConstants.NO_POSITIVE_NUMBER_KEY_ERROR_MESSAGE;
+import static com.epam.cargo.exception.WrongInputDataKeysConstants.REQUIRED_KEY_ERROR_MESSAGE;
+
 @Getter
 @Setter
 public class DeliveryCostCalculatorRequest {
+
+    @NotNull(message = REQUIRED_KEY_ERROR_MESSAGE)
     private Long cityFromId;
+
+    @NotNull(message = REQUIRED_KEY_ERROR_MESSAGE)
     private Long cityToId;
 
+    @Valid
     private DimensionsRequest dimensions;
+
+    @NotNull(message = REQUIRED_KEY_ERROR_MESSAGE)
+    @Positive(message = NO_POSITIVE_NUMBER_KEY_ERROR_MESSAGE)
     private Double weight;
 
     public static DeliveryCostCalculatorRequest of(Long cityFromId, Long cityToId, DimensionsRequest dimensions, Double weight){
--- src/main/java/com/epam/cargo/dto/DimensionsRequest.java
+++ src/main/java/com/epam/cargo/dto/DimensionsRequest.java
@@ -3,14 +3,28 @@ package com.epam.cargo.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.Objects;
 import java.util.function.Supplier;
+
+import static com.epam.cargo.exception.WrongInputDataKeysConstants.NO_POSITIVE_NUMBER_KEY_ERROR_MESSAGE;
+import static com.epam.cargo.exception.WrongInputDataKeysConstants.REQUIRED_KEY_ERROR_MESSAGE;
 
 @Getter
 @Setter
 public class DimensionsRequest {
+
+    @NotNull(message = REQUIRED_KEY_ERROR_MESSAGE)
+    @Positive(message = NO_POSITIVE_NUMBER_KEY_ERROR_MESSAGE)
     private Double length;
+
+    @NotNull(message = REQUIRED_KEY_ERROR_MESSAGE)
+    @Positive(message = NO_POSITIVE_NUMBER_KEY_ERROR_MESSAGE)
     private Double width;
+
+    @NotNull(message = REQUIRED_KEY_ERROR_MESSAGE)
+    @Positive(message = NO_POSITIVE_NUMBER_KEY_ERROR_MESSAGE)
     private Double height;
 
     /**
