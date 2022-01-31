9eb0195eaece19fd0fd61dd336ba7cdedd0418a3
Fed333 <kovalchuk.roman03@gmail.com>
2022-02-01 00:18:33 +0200

    added ServiceUtils for taking out some needed logic

---
+++ src/main/java/com/epam/cargo/service/ServiceUtils.java
@@ -0,0 +1,89 @@
+package com.epam.cargo.service;
+
+import com.epam.cargo.dto.AddressRequest;
+import com.epam.cargo.dto.DeliveredBaggageRequest;
+import com.epam.cargo.dto.DeliveryApplicationRequest;
+import com.epam.cargo.entity.*;
+import com.epam.cargo.exception.NoExistingCityException;
+
+import java.util.Objects;
+
+public class ServiceUtils {
+
+    static DeliveredBaggage createDeliveredBaggage(DeliveredBaggageRequest request){
+        Objects.requireNonNull(request);
+
+        DeliveredBaggage object = new DeliveredBaggage();
+
+        object.setDescription(request.getDescription());
+        object.setType(Objects.requireNonNull(request.getType()));
+        object.setVolume(Objects.requireNonNull(request.getVolume()));
+        object.setWeight(Objects.requireNonNull(request.getWeight()));
+
+        return object;
+    }
+
+    /**
+     * makes an object according to dto request object
+     * @param customer owner of application
+     * @param request web dto request object
+     * @param cityService service of City entity
+     * @return the object with initialized fields
+     * @throws NoExistingCityException if any given city isn't present in the database
+     * @throws NullPointerException if any argument is null
+     * */
+    static DeliveryApplication createDeliveryApplication(User customer, DeliveryApplicationRequest request, CityService cityService) throws NoExistingCityException {
+        Objects.requireNonNull(request);
+        Objects.requireNonNull(cityService);
+
+        DeliveryApplication object = new DeliveryApplication();
+        object.setCustomer(Objects.requireNonNull(customer));
+
+        object.setDeliveredBaggage(createDeliveredBaggage(request.getDeliveredBaggageRequest()));
+
+        Address senderAddress = createAddress(request.getSenderAddress(), cityService);
+        Address receiverAddress = createAddress(request.getReceiverAddress(), cityService);
+
+        requireDifferentAddresses(senderAddress, receiverAddress);
+
+        object.setSenderAddress(senderAddress);
+        object.setReceiverAddress(receiverAddress);
+
+        object.setSendingDate(Objects.requireNonNull(request.getSendingDate()));
+        object.setReceivingDate(request.getReceivingDate());
+
+        return object;
+    }
+
+    private static void requireDifferentAddresses(Address senderAddress, Address receiverAddress) {
+        if (senderAddress.equals(receiverAddress)){
+            throw new IllegalArgumentException("Sender and Receiver address mustn't be equal!");
+        }
+    }
+
+    static Address createAddress(AddressRequest request, CityService cityService) throws NoExistingCityException {
+        Objects.requireNonNull(request);
+
+        Address object = new Address();
+        object.setCity(requireExistingCity(request, cityService));
+        object.setStreet(request.getStreetName());
+        object.setHouseNumber(request.getHouseNumber());
+
+        return object;
+    }
+
+
+    private static City requireExistingCity(AddressRequest request, CityService cityService) throws NoExistingCityException {
+        City city = cityService.findCityById(request.getCityId());
+        if (Objects.isNull(city)){
+            throw new NoExistingCityException();
+        }
+        return city;
+    }
+
+    static void requireExistingUser(User user, UserService userService){
+        if (Objects.isNull(userService.findUserByLogin(user.getLogin()))){
+            throw new IllegalArgumentException("User " + user + " must be present in database");
+        }
+    }
+}
