9ce79554c5241b89eadee0dc654e28a9f7cece8c
Fed333 <kovalchuk.roman03@gmail.com>
2022-02-02 02:29:43 +0200

    added baggage types and cities to the model

--- src/main/java/com/epam/cargo/controller/DeliveryApplicationController.java
+++ src/main/java/com/epam/cargo/controller/DeliveryApplicationController.java
@@ -1,11 +1,14 @@
 package com.epam.cargo.controller;
 
 import com.epam.cargo.dto.DeliveryApplicationRequest;
+import com.epam.cargo.entity.BaggageType;
 import com.epam.cargo.entity.DeliveryApplication;
 import com.epam.cargo.entity.User;
 import com.epam.cargo.exception.WrongDataException;
+import com.epam.cargo.service.CityService;
 import com.epam.cargo.service.DeliveryApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/make_app")
@@ -24,22 +28,33 @@ public class DeliveryApplicationController {
     @Autowired
     private DeliveryApplicationService applicationService;
 
+    @Autowired
+    public CityService cityService;
+
     @GetMapping
     public String makeApplicationPage(
             @AuthenticationPrincipal User customer,
             DeliveryApplicationRequest applicationRequest,
             BindingResult bindingResult,
-            Model model
+            Model model,
+            Locale locale
     ){
         if (!bindingResult.hasErrors()) {
 
             List<DeliveryApplication> applications = applicationService.findAll();
             applications.forEach(System.out::println);
+
+
         }
         else{
+
             //TODO errors handling
 
         }
+
+        model.addAttribute("types", BaggageType.values());
+        model.addAttribute("cities", cityService.findAll(locale, Sort.by(Sort.Order.by("name"))));
+
         return "makeDeliveryApplication";
     }
 
