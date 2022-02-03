855ed809f2f524976ea9d3481cf9b708f15c46c3
Fed333 <kovalchuk.roman03@gmail.com>
2022-02-03 02:53:47 +0200

    added calculatePrice method to retrieving total cost of delivery application assigning this value to application before saving in database

--- src/main/java/com/epam/cargo/service/DeliveryApplicationService.java
+++ src/main/java/com/epam/cargo/service/DeliveryApplicationService.java
@@ -1,9 +1,11 @@
 package com.epam.cargo.service;
 
 import com.epam.cargo.dto.DeliveryApplicationRequest;
+import com.epam.cargo.entity.DeliveredBaggage;
 import com.epam.cargo.entity.DeliveryApplication;
 import com.epam.cargo.entity.User;
 import com.epam.cargo.exception.NoExistingCityException;
+import com.epam.cargo.exception.NoExistingDirectionException;
 import com.epam.cargo.exception.WrongDataException;
 import com.epam.cargo.repos.DeliveryApplicationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,14 +32,25 @@ public class DeliveryApplicationService {
     @Autowired
     private AddressService addressService;
 
+    @Autowired
+    private DeliveryCostCalculatorService costCalculatorService;
+
     public boolean sendApplication(User customer, DeliveryApplicationRequest request) throws WrongDataException {
         Objects.requireNonNull(customer, "Customer object cannot be null");
         Objects.requireNonNull(request, "DeliveryApplicationRequest object cannot be null");
 
         DeliveryApplication application = ServiceUtils.createDeliveryApplication(customer, request, cityService);
-
+        application.setPrice(calculatePrice(application));
         return sendApplication(application);
 
+    }
+
+    private Double calculatePrice(DeliveryApplication application) throws NoExistingDirectionException {
+        Double distanceCost = costCalculatorService.calculateDistanceCost(application.getSenderAddress(), application.getReceiverAddress());
+        DeliveredBaggage deliveredBaggage = application.getDeliveredBaggage();
+        Double weightCost = costCalculatorService.calculateWeightCost(deliveredBaggage.getWeight());
+        Double dimensionsCost = costCalculatorService.calculateDimensionsCost(deliveredBaggage.getVolume());
+        return distanceCost + weightCost + dimensionsCost;
     }
 
     public boolean sendApplication(DeliveryApplication application) throws NoExistingCityException {
