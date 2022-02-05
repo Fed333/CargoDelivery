d53327ad5b48e23c600963e79878badc0f16d8d8
Fed333 <kovalchuk.roman03@gmail.com>
2022-02-05 19:25:15 +0200

    added getting applications page added some needed attributes to the model

--- src/main/java/com/epam/cargo/controller/DeliveryApplicationsReviewController.java
+++ src/main/java/com/epam/cargo/controller/DeliveryApplicationsReviewController.java
@@ -1,10 +1,16 @@
 package com.epam.cargo.controller;
 
 import com.epam.cargo.dto.DeliveryApplicationsReviewFilterRequest;
+import com.epam.cargo.entity.BaggageType;
 import com.epam.cargo.entity.DeliveryApplication;
 import com.epam.cargo.entity.User;
+import com.epam.cargo.service.CityService;
 import com.epam.cargo.service.DeliveryApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -12,7 +18,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 import java.util.Locale;
 
 @Controller
@@ -23,15 +28,25 @@ public class DeliveryApplicationsReviewController {
     @Autowired
     private DeliveryApplicationService applicationService;
 
+    @Autowired
+    private CityService cityService;
+
     @GetMapping
     public String reviewApplications(
             @AuthenticationPrincipal User manager,
             DeliveryApplicationsReviewFilterRequest applicationsRequest,
+            @PageableDefault(size = 6, sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable,
             Model model,
             Locale locale
     ){
-        List<DeliveryApplication> applications = applicationService.findAll(applicationsRequest);
-        model.addAttribute("applications", applications);
+        Page<DeliveryApplication> applications = applicationService.getPage(applicationsRequest, pageable);
+        model.addAttribute("applicationsPage", applications);
+        model.addAttribute("states", DeliveryApplication.State.values());
+        model.addAttribute("types", BaggageType.values());
+        model.addAttribute("cities", cityService.findAll(locale));
+        model.addAttribute("url", "/applications/review");
+        model.addAttribute("lang", locale.getLanguage());
+
         return "applicationsReview";
     }
 }
