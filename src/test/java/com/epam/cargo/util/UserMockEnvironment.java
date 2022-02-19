5dca1cc9ccf51387ca9b50da2f36e10f43dc6dbe
Fed333 <kovalchuk.roman03@gmail.com>
2022-02-19 21:57:59 +0200

    added util class for mocking UserRepo

---
+++ src/test/java/com/epam/cargo/util/UserMockEnvironment.java
@@ -0,0 +1,102 @@
+package com.epam.cargo.util;
+
+import com.epam.cargo.entity.User;
+import com.epam.cargo.repos.UserRepo;
+import lombok.Getter;
+import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+/**
+ * Designed specially for mocking user's db repositories
+ * So far mocks only User itself, doesn't mock mapped db relationship
+ * Doesn't regard given User id, assigns current value of id's counter along saving
+ * @author Roman Kovalchuk
+ * @since 19.02.2021
+ * */
+public class UserMockEnvironment {
+
+    private Long id = 1L;
+
+    @Getter
+    private Map<Long, User> userStorageById = new HashMap<>();
+    @Getter
+    private Map<String, User> userStorageByLogin = new HashMap<>();
+
+    public void mockFindById(UserRepo repo){
+        Mockito.when(repo.findById(any(Long.class)))
+                .thenAnswer(findByIdAnswer());
+    }
+
+    public void mockFindAll(UserRepo repo){
+        Mockito.when(repo.findAll())
+                .thenAnswer(findAllAnswer());
+    }
+
+    public void mockFindByLogin(UserRepo repo){
+        Mockito.when(repo.findByLogin(anyString()))
+                .thenAnswer(findByLoginAnswer());
+    }
+
+    public void mockSave(UserRepo repo){
+        Mockito.when(repo.save(any(User.class)))
+                .thenAnswer(saveAnswer());
+    }
+
+    public void mockDelete(UserRepo repo){
+         Mockito.doAnswer(invocationOnMock -> {
+             User user = invocationOnMock.getArgument(0, User.class);
+             userStorageById.remove(user.getId());
+             userStorageByLogin.remove(user.getLogin());
+            return null;
+         }).when(repo).delete(any(User.class));
+    }
+
+    private Answer<Object> saveAnswer() {
+        return invocationOnMock -> {
+            User user = invocationOnMock.getArgument(0, User.class);
+            requireValidUser(user);
+            user.setId(id++);
+            userStorageById.put(user.getId(), user);
+            userStorageByLogin.put(user.getLogin(), user);
+            return user;
+        };
+    }
+
+    private void requireValidUser(User user) {
+        Objects.requireNonNull(user.getLogin());
+    }
+
+    private Answer<Object> findByLoginAnswer() {
+        return invocationOnMock -> {
+            String login = invocationOnMock.getArgument(0, String.class);
+            return userStorageByLogin.get(login);
+        };
+    }
+
+    private Answer<Object> findAllAnswer() {
+        return invocationOnMock -> userStorageById.values();
+    }
+
+    private Answer<Object> findByIdAnswer() {
+        return invocationOnMock -> {
+            Long id = invocationOnMock.getArgument(0, Long.class);
+            return Optional.ofNullable(userStorageById.get(id));
+        };
+    }
+
+    public void mockUserRepoBean(UserRepo userRepo) {
+        mockSave(userRepo);
+        mockFindAll(userRepo);
+        mockFindById(userRepo);
+        mockFindByLogin(userRepo);
+        mockDelete(userRepo);
+    }
+}
