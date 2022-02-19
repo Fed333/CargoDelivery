1c2e4e79c32ba8416734bbef82f69bf3d5ce2c91
Fed333 <kovalchuk.roman03@gmail.com>
2022-02-19 04:02:15 +0200

    made fare classes for builder pattern

--- src/main/java/com/epam/cargo/entity/DimensionsFare.java
+++ src/main/java/com/epam/cargo/entity/DimensionsFare.java
@@ -1,5 +1,6 @@
 package com.epam.cargo.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,18 +10,35 @@ import javax.persistence.*;
 @Table(name = "dimensions_fares")
 @Getter
 @Setter
+@Builder
 public class DimensionsFare {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * lower bound of fare (inclusive)
+     * */
     @Column(name = "dimensions_from")
     private Integer dimensionsFrom;
 
+    /**
+     * upper bound of fare (inclusive)
+     * */
     @Column(name = "dimensions_to")
     private Integer dimensionsTo;
 
     @Column(name = "price")
     private Double price;
+
+    public DimensionsFare() {
+    }
+
+    public DimensionsFare(Long id, Integer dimensionsFrom, Integer dimensionsTo, Double price) {
+        this.id = id;
+        this.dimensionsFrom = dimensionsFrom;
+        this.dimensionsTo = dimensionsTo;
+        this.price = price;
+    }
 }
--- src/main/java/com/epam/cargo/entity/DistanceFare.java
+++ src/main/java/com/epam/cargo/entity/DistanceFare.java
@@ -1,5 +1,6 @@
 package com.epam.cargo.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,18 +10,35 @@ import javax.persistence.*;
 @Table(name = "distance_fares")
 @Getter
 @Setter
+@Builder
 public class DistanceFare {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * lower bound of fare (inclusive)
+     * */
     @Column(name = "distance_from")
     private Integer distanceFrom;
 
+    /**
+     * upper bound of fare (inclusive)
+     * */
     @Column(name = "distance_to")
     private Integer distanceTo;
 
     @Column(name = "price")
     private Double price;
 
+    public DistanceFare() {
+
+    }
+
+    public DistanceFare(Long id, Integer distanceFrom, Integer distanceTo, Double price) {
+        this.id = id;
+        this.distanceFrom = distanceFrom;
+        this.distanceTo = distanceTo;
+        this.price = price;
+    }
 }
