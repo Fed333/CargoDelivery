330c21cb8ff671896618c7433b8af123d48b3591
Fed333 <kovalchuk.roman03@gmail.com>
2022-05-14 14:47:03 +0300

    wrote jpql query for baggage per type review

--- src/main/java/com/epam/cargo/repos/DeliveredBaggageRepo.java
+++ src/main/java/com/epam/cargo/repos/DeliveredBaggageRepo.java
@@ -1,7 +1,11 @@
 package com.epam.cargo.repos;
 
+import com.epam.cargo.custom.AmountBaggagePerType;
 import com.epam.cargo.entity.DeliveredBaggage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Repository of fetching DeliveredBaggage objects from database.
@@ -10,4 +14,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * */
 public interface DeliveredBaggageRepo extends JpaRepository<DeliveredBaggage, Long> {
 
+    @Query("SELECT " +
+            "new com.epam.cargo.custom.AmountBaggagePerType(b.type, COUNT(b.type))" +
+            "FROM DeliveredBaggage b " +
+            "GROUP BY b.type")
+    List<AmountBaggagePerType> countBaggagePerType();
 }
