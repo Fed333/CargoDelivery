ec6c98e1ea6a38718be314f2c22206ba8376ec03
Fed333 <kovalchuk.roman03@gmail.com>
2022-05-14 20:42:44 +0300

    implemented getting profit baggage per type in business logic

--- src/main/java/com/epam/cargo/controller/BaggageReviewController.java
+++ src/main/java/com/epam/cargo/controller/BaggageReviewController.java
@@ -1,6 +1,7 @@
 package com.epam.cargo.controller;
 
 import com.epam.cargo.service.AmountBaggagePerTypeService;
+import com.epam.cargo.service.DeliveredBaggageService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +12,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BaggageReviewController {
 
     private final AmountBaggagePerTypeService baggagePerTypeService;
-    private final String BAGGAGE_PER_TYPE_REPORT = "baggagePerTypeReport";
+
+    private final DeliveredBaggageService baggageService;
+
+    private final String AMOUNT_BAGGAGE_PER_TYPE_REPORT = "amountBaggagePerTypeReport";
+    private final String PROFIT_BAGGAGE_PER_TYPE_REPORT = "profitBaggagePerTypeReport";
 
     @GetMapping("/baggage/review")
     public String baggageReviewPage(
             Model model
     ) {
-        model.addAttribute(BAGGAGE_PER_TYPE_REPORT, baggagePerTypeService.getBaggageReport());
+        model.addAttribute(AMOUNT_BAGGAGE_PER_TYPE_REPORT, baggagePerTypeService.getBaggageReport());
+        model.addAttribute(PROFIT_BAGGAGE_PER_TYPE_REPORT, baggageService.profitBaggageReport());
+
         return "baggageReview";
     }
-
 }
